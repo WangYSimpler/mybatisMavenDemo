cf6cf084e80da2e66f076ac01f57398af31ab968
WangYSimpler <wangyongsd2011@163.com>
2016-07-14 00:16:08 +0800

    demo/controller/HomeController.class

---
+++ src/main/java/repo/xirong/java/demo/service/TestService.java
@@ -0,0 +1,10 @@
+package repo.xirong.java.demo.service;
+
+import repo.xirong.java.demo.model.User;
+
+import java.util.ArrayList;
+
+
+public interface TestService {
+	public ArrayList<User> getAllUsers();
+}
